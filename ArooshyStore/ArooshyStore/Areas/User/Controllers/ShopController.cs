6d17ec85c1459fc1a52604a8f3768668cc7ce52b
Jabbaria011 <141347185+JabbariaHanif@users.noreply.github.com>
2024-10-31 02:48:21 +0500

    branch 8

--- ArooshyStore/ArooshyStore/Areas/User/Controllers/ShopController.cs
+++ ArooshyStore/ArooshyStore/Areas/User/Controllers/ShopController.cs
@@ -49,17 +49,12 @@ namespace ArooshyStore.Areas.User.Controllers
             return PartialView(model);
         }
         public ActionResult SetSearchString(bool? categoryCheckbox, int[] category,bool? attributeCheckbox, int[] attribute,
-            bool? discountCheckbox, int[] discount, decimal? minPrice, decimal? maxPrice, string sortBy, string viewType)
+            bool? discountCheckbox, int[] discount, decimal? minPrice, decimal? maxPrice, string sortBy)
         {
             var filteredProducts = _repository.GetFilteredProducts(categoryCheckbox, category,attributeCheckbox, attribute, discountCheckbox, discount, minPrice, maxPrice, sortBy);
-            if (viewType == "list")
-            {
-                return PartialView("GetProductsList", filteredProducts); // Ensure this view exists
-            }
-            else
-            {
-                return PartialView("GetProductsGrid", filteredProducts); // Existing grid view
-            }
+            
+                return PartialView("GetProductsGrid", filteredProducts); 
+            
         }
 
     }
